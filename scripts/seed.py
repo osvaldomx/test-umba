4c94f45e3757284f1968d97922f862d247efdb08
Osvaldo Trujillo <osvaldotrujillo89@gmail.com>
2021-10-15 16:01:31 -0500

    [FIX] pylint punctation 10/10 stage 1

--- scripts/seed.py
+++ scripts/seed.py
@@ -1,134 +1,156 @@
+"""
+Stage 1
+
+Create a python shell script called seed.py wiche only responsability will be
+to populate and SQLite database, with the information of the GitHub Users API
+into a github_users table. The required fields will be:
+
+* id, username, avatar_url, type, URL
+"""
+
 import sys
 import getopt
-import requests
-import sqlite3
 
+import sqlite3
 from sqlite3 import Error
 
+import requests
+
 def setup_db(db_file):
-	"""
-	Function to create connection to sqlite databese and create table users.
+    """
+    Function to create connection to sqlite databese and create table users.
 
-	Parameters
-	----------
-	db_file : str
-		Path and name of sqldatabase.
+    Parameters
+    ----------
+    db_file : str
+        Path and name of sqldatabase.
 
-	Raises
-	------
-	Exception
-		If can't create connection to database.
+    Raises
+    ------
+    Exception
+        If can't create connection to database.
 
-	Returns
-	-------
-	conection : Connection Object
-	"""
-	
-	connection = None
+    Returns
+    -------
+    conection : Connection Object
+    """
 
-	try:
-		connection = sqlite3.connect(db_file)
-		#print(sqlite3.version)
-		#print("Connection to databese created...")
-	except Exception as e:
-		raise e
+    connection = None
 
-	create_table = """
-						CREATE TABLE IF NOT EXISTS users (
-							id integer PRIMARY KEY,
-							username text NOT NULL,
-							avatar_url text,
-							type text NOT NULL,
-							url text NOT NULL
-							);
-	"""
+    try:
+        connection = sqlite3.connect(db_file)
+    except Exception as exc:
+        raise exc
 
-	try:
-		c = connection.cursor()
-		c.execute(create_table)
-		#print("Database and table 'users' setup up...")
-	except Error as e:
-		raise e
+    create_table = """
+                        CREATE TABLE IF NOT EXISTS users (
+                            id integer PRIMARY KEY,
+                            username text NOT NULL,
+                            avatar_url text,
+                            type text NOT NULL,
+                            url text NOT NULL
+                            );
+    """
 
-	return connection
+    try:
+        cursor = connection.cursor()
+        cursor.execute(create_table)
+    except Error as err:
+        raise err
 
-	
+    return connection
+
+def get_data(list_pages):
+    """
+    Function to get users from github API.
+
+    Parameters
+    ----------
+    total_records : int
+        Total of records to get from github.
+    list_pages : list
+        List of limit of records per page.
+
+    Retruns
+    -------
+    data : list
+        List with total of user records.
+    """
+    data = []
+    since = 0
+
+    for page in list_pages:
+        json = requests.get("https://api.github.com/users?per_page="\
+                                + str(page)\
+                                + "&since="\
+                                + str(since)).json()
+        data.append(json)
+        since += json[-1]['id']
+
+    return data
+
 
 def populate_table(conn, *args):
-	"""
-	Function to populate users table.
-	
-	Parameters
-	----------
-	conn : Connection Object
-		Object to execute queries.
-	args : tuple
-		Tuple with arguments passed from console.
+    """
+    Function to populate users table.
 
-	Arguments
-	---------
-	-t, --total
-		Total of records to populate the database. By default 150.
-	"""
-	
-	total = 150
-	args = args[0]
+    Parameters
+    ----------
+    conn : Connection Object
+        Object to execute queries.
+    args : tuple
+        Tuple with arguments passed from console.
 
-	try:
-		optlist, args = getopt.getopt(args, 't:', ['total='])
+    Arguments
+    ---------
+    -t, --total
+        Total of records to populate the database. By default 150.
+    """
 
-	except getopt.GetoptError as err:
-		print(err)
-		print(populate_table.__doc__[178:274])
-		sys.exit(2)
+    total = 150
 
-	for opt, val in optlist:
-		if opt in ['-t', '--total']:
-			total = int(val)
-			print("The database will populate with {} records".format(total))
+    try:
+        optlist, args = getopt.getopt(args[0], 't:', ['total='])
 
-	per_page = 100
-	per_page_list = []
-	since = 0
+    except getopt.GetoptError as err:
+        print(err)
+        print(populate_table.__doc__[178:274])
+        sys.exit(2)
 
-	if total <= per_page:
-		per_page_list.append(total)
-	else:
-		per_page_list = [100] * int(total / per_page)
-		mod = total % per_page
-		if mod > 0:
-			per_page_list.append(mod)
+    for opt, val in optlist:
+        if opt in ['-t', '--total']:
+            total = int(val)
 
-	data = []
+    per_page = 100
+    per_page_list = []
 
-	for page in per_page_list:
-		#print(page, since)
-		json = requests.get("https://api.github.com/users?per_page="\
-								+ str(page)\
-								+ "&since="\
-								+ str(since)).json()
-		data.append(json)
-		since += json[-1]['id']
+    if total <= per_page:
+        per_page_list.append(total)
+    else:
+        per_page_list = [100] * int(total / per_page)
+        if total % per_page > 0:
+            per_page_list.append(total % per_page)
 
-	insert = "INSERT INTO users VALUES (?,?,?,?,?);"
-	c = conn.cursor()
+    data = get_data(per_page_list)
 
-	for page in data:
-		for element in page:
-			user = (element['id'],
-					element['login'],
-					element['avatar_url'],
-					element['type'],
-					element['html_url'])
-			#print(user)
-			c.execute(insert, user)
+    insert = "INSERT INTO users VALUES (?,?,?,?,?);"
+    cursor = conn.cursor()
 
-	conn.commit()
+    for page in data:
+        for element in page:
+            user = (element['id'],
+                    element['login'],
+                    element['avatar_url'],
+                    element['type'],
+                    element['html_url'])
+            cursor.execute(insert, user)
 
-	return True
+    conn.commit()
+
+    return True
 
 
 if __name__ == '__main__':
-	conn = setup_db("./users.db")
-	populate_table(conn, sys.argv[1:])
-	conn.close()
+    connection_obj = setup_db("./users.db")
+    populate_table(connection_obj, sys.argv[1:])
+    connection_obj.close()
--- scripts/test.py
+++ scripts/test.py
@@ -1,17 +1,36 @@
+"""
+Tests for seed.py
+"""
 import unittest
 import sqlite3
 
 from seed import setup_db
+from seed import get_data
 from seed import populate_table
 
 class TestSeed(unittest.TestCase):
+    """
+    Class for tests
+    """
+    def test_setup_db(self):
+        """
+        Test function 'setup_db'
+        """
+        self.assertIsInstance(setup_db("test.db"), sqlite3.Connection)
 
-	def test_setup_db(self):
-		self.assertIsInstance(setup_db("test.db"), sqlite3.Connection)
+    def test_get_data(self):
+        """
+        Test function 'get_data'
+        """
+        self.assertIsInstance(get_data([10]), list)
 
-	def test_populate_table(self):
-		conn = setup_db("test.db")
-		assert populate_table(conn, (['x','-t','10'])) == True
+    @staticmethod
+    def test_populate_table():
+        """
+        Test function 'populate_table'
+        """
+        conn = setup_db("test.db")
+        assert populate_table(conn, (['x','-t','10'])) is True
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
