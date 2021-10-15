c6265ffb9c97d2315183ff1a46e478dc6f104b3e
Osvaldo Trujillo <osvaldotrujillo89@gmail.com>
2021-10-15 02:36:44 -0500

    [FIX] bug in line 112

--- scripts/seed.py
+++ scripts/seed.py
@@ -89,121 +89,40 @@ def populate_table(conn, *args):
 			print("The database will populate with {} records".format(total))
 
 	per_page = 100
+	per_page_list = []
 	since = 0
-	limit = int(total/per_page) + 1
 
-	#for i in range(limit):
-	#	json = requests.get("https://api.github.com/users?per_page="\
-	#							+ str(per_page)\
-	#							+ "&since="\
-	#							+ str(since)).json()
-	#	since += 100
+	if total <= per_page:
+		per_page_list.append(total)
+	else:
+		per_page_list = [100] * int(total / per_page)
+		mod = total % per_page
+		if mod > 0:
+			per_page_list.append(mod)
 
-	json = [
-			{
-				"login": "octocat",
-			  	"id": 1,
-				"node_id": "MDQ6VXNlcjE=",
-				"avatar_url": "https://github.com/images/error/octocat_happy.gif",
-				"gravatar_id": "",
-				"url": "https://api.github.com/users/octocat",
-				"html_url": "https://github.com/octocat",
-				"followers_url": "https://api.github.com/users/octocat/followers",
-				"following_url": "https://api.github.com/users/octocat/following{/other_user}",
-				"gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
-				"starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
-				"subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
-				"organizations_url": "https://api.github.com/users/octocat/orgs",
-				"repos_url": "https://api.github.com/users/octocat/repos",
-				"events_url": "https://api.github.com/users/octocat/events{/privacy}",
-				"received_events_url": "https://api.github.com/users/octocat/received_events",
-				"type": "User",
-				"site_admin": False,
-				"name": "monalisa octocat",
-				"company": "GitHub",
-				"blog": "https://github.com/blog",
-				"location": "San Francisco",
-				"email": "octocat@github.com",
-				"hireable": False,
-				"bio": "There once was...",
-				"twitter_username": "monatheoctocat",
-				"public_repos": 2,
-				"public_gists": 1,
-				"followers": 20,
-				"following": 0,
-				"created_at": "2008-01-14T04:33:35Z",
-				"updated_at": "2008-01-14T04:33:35Z",
-				"private_gists": 81,
-				"total_private_repos": 100,
-				"owned_private_repos": 100,
-				"disk_usage": 10000,
-				"collaborators": 8,
-				"two_factor_authentication": True,
-				"plan": {
-					"name": "Medium",
-					"space": 400,
-					"private_repos": 20,
-					"collaborators": 0
-				}
-			},
-			{
-				"login": "octocat",
-			  	"id": 2,
-				"node_id": "MDQ6VXNlcjE=",
-				"avatar_url": "https://github.com/images/error/octocat_happy.gif",
-				"gravatar_id": "",
-				"url": "https://api.github.com/users/octocat",
-				"html_url": "https://github.com/octocat",
-				"followers_url": "https://api.github.com/users/octocat/followers",
-				"following_url": "https://api.github.com/users/octocat/following{/other_user}",
-				"gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
-				"starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
-				"subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
-				"organizations_url": "https://api.github.com/users/octocat/orgs",
-				"repos_url": "https://api.github.com/users/octocat/repos",
-				"events_url": "https://api.github.com/users/octocat/events{/privacy}",
-				"received_events_url": "https://api.github.com/users/octocat/received_events",
-				"type": "User",
-				"site_admin": False,
-				"name": "monalisa octocat",
-				"company": "GitHub",
-				"blog": "https://github.com/blog",
-				"location": "San Francisco",
-				"email": "octocat@github.com",
-				"hireable": False,
-				"bio": "There once was...",
-				"twitter_username": "monatheoctocat",
-				"public_repos": 2,
-				"public_gists": 1,
-				"followers": 20,
-				"following": 0,
-				"created_at": "2008-01-14T04:33:35Z",
-				"updated_at": "2008-01-14T04:33:35Z",
-				"private_gists": 81,
-				"total_private_repos": 100,
-				"owned_private_repos": 100,
-				"disk_usage": 10000,
-				"collaborators": 8,
-				"two_factor_authentication": True,
-				"plan": {
-					"name": "Medium",
-					"space": 400,
-					"private_repos": 20,
-					"collaborators": 0
-				}
-			}
-	]
+	data = []
+
+	for page in per_page_list:
+		#print(page, since)
+		json = requests.get("https://api.github.com/users?per_page="\
+								+ str(page)\
+								+ "&since="\
+								+ str(since)).json()
+		data.append(json)
+		since += json[-1]['id']
 
 	insert = "INSERT INTO users VALUES (?,?,?,?,?);"
 	c = conn.cursor()
 
-	for element in json:
-		user = (element['id'],
-				element['login'],
-				element['avatar_url'],
-				element['type'],
-				element['html_url'])
-		c.execute(insert, user)
+	for page in data:
+		for element in page:
+			user = (element['id'],
+					element['login'],
+					element['avatar_url'],
+					element['type'],
+					element['html_url'])
+			#print(user)
+			c.execute(insert, user)
 
 	conn.commit()
 
